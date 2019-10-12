76e083ac94b1618bd4a3640d98d44a6e61410706
swadharai <swadha@live.unc.edu>
2019-10-12 14:31:42 -0400

    Initial Commit

---
+++ SwipeSwapDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeSwapDemo
+//
+//  Created by Swadha Rai on 10/12/19.
+//  Copyright © 2019 Swipers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
